4c39ea8b9c4824ee00ebf1bbc5854687f59516c7
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:36:48 +0530

    creating loop to calculate difference between adjacent elements

--- NeighboringNumbers.java
+++ NeighboringNumbers.java
@@ -3,12 +3,21 @@
 //finding neighboring numbers with the smallest distance
 public class NeighboringNumbers {
     public int findSmallestDistance(int[] array) {
-        if (aary.length < 2) {
+        if (array.length < 2) {
             throw new IllegalArgumentException("Array length should be at least 2");
         }
 
         int minDistance = Integer.MAX_VALUE;
         int index = -1;
 
+        // loop to calculate difference between adjacent elements
+        for (int i = 0; i < array.length - 1; i++) {
+            int distance = Math.abs(array[i] - array[i + 1]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                index = i;
+            }
+        }
+        return index;
     }
 }
