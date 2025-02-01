935170664fbf4eee79555c4ea958af4b5582872d
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:46:53 +0530

    creating main file

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Main.java
+//Name: Disha Deepak Gupta
+//PRN: 24070126513
+//Batch: A2
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
