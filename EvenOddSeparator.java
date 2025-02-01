78aee804d0ff79c735f14ccef293c01538d0c3aa
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:20:09 +0530

    separating even and odd numbers into different arrays

--- EvenOddSeparator.java
+++ EvenOddSeparator.java
@@ -9,5 +9,20 @@ public class EvenOddSeparator {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> even = new ArrayList<Integer>();
         ArrayList<Integer> odd = new ArrayList<Integer>();
+
+        System.out.println("Enter numbers (type -1 to stop): ");
+        while (true) {
+            int num = scanner.nextInt();
+            if (num == -1) {
+                break;
+            }
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        System.out.println("Array of even numbers: " + even);
+        System.out.println("Array of odd numbers: " + odd);
     }
 }
