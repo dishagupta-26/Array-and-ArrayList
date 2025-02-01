958bbf895f9a2c85c40726f84042d01ebc010328
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:44:07 +0530

    arrayList to array converter

--- ArrayConverter.java
+++ ArrayConverter.java
@@ -5,12 +5,19 @@ import java.util.Arrays;
 
 //class for converting array to ArrayList and vice versa
 public class ArrayConverter {
-    public ArrayList<Integer> convertArrayToList(int[] arr) {
+    public ArrayList<Integer> convertArrayToList(int[] array) {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        for (int num : arr) {
+        for (int num : array) {
             arrayList.add(num);
         }
         return arrayList;
     }
 
+    public int[] arrayListToArray(ArrayList<Integer> arrayList) {
+        int[] array = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            array[i] = arrayList.get(i);
+        }
+        return array;
+    }
 }
