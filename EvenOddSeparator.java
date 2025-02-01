07b93382e014acf0d8bddd2609df559bc9b5ec47
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:16:01 +0530

    initializing even and odd array list

--- EvenOddSeparator.java
+++ EvenOddSeparator.java
@@ -1,1 +1,13 @@
 // EvenOddSeparator.java
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//class for separating even and odd numbers into different arrays
+public class EvenOddSeparator {
+    public void separateEvenOdd(int[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> even = new ArrayList<Integer>();
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+    }
+}
