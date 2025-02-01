27ba2259a917593c951fe8c294697a6210eff043
Disha Gupta <gupta.disha.d26@gmail.com>
2025-02-01 19:34:36 +0530

    defining exception for length

--- NeighboringNumbers.java
+++ NeighboringNumbers.java
@@ -1,5 +1,14 @@
 //NeighboringNumbers.java
 
+//finding neighboring numbers with the smallest distance
 public class NeighboringNumbers {
+    public int findSmallestDistance(int[] array) {
+        if (aary.length < 2) {
+            throw new IllegalArgumentException("Array length should be at least 2");
+        }
 
+        int minDistance = Integer.MAX_VALUE;
+        int index = -1;
+
+    }
 }
